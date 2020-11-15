f42cccc6f2b7a9040637b66ea82c4e81c5437be9
pmecchia <mecchia.pierre@gmail.com>
2020-11-15 18:25:16 +0100

    correction of column name and traces titles position

--- components/confirmed_chart.py
+++ components/confirmed_chart.py
@@ -10,7 +10,7 @@ def confirmed_cases(department,last_days_nb):
     else:
         selected_df=DF_DEPARTMENTS[DF_DEPARTMENTS["dep_name"]==department]
         
-    yaxis_max=np.log10(selected_df['nb_pos_cum'].tail(1).tolist()[0])+0.5    
+    yaxis_max=np.log10(selected_df['Confirmed'].tail(1).tolist()[0])+0.5    
     fig_confirmed = go.Figure()
     fig_confirmed.add_trace(
         go.Scatter(
@@ -26,7 +26,7 @@ def confirmed_cases(department,last_days_nb):
         go.Bar(
             x=selected_df['jour'].tail(last_days_nb),
             y=selected_df['nb_pos'].tail(last_days_nb),
-            marker={"color":"#dc9e00"},
+            marker={"color":"#f9d67a"},
             showlegend=False,
             #name="New Confirmed Cases",
 
@@ -36,22 +36,22 @@ def confirmed_cases(department,last_days_nb):
 
     # LINE CHART ANNOTATION
     fig_confirmed.add_annotation(
-         x=selected_df['jour'].tail(1).tolist()[0],
+         x=selected_df['jour'].tail(last_days_nb).tolist()[0],
          y=yaxis_max,
          text="Total Confirmed Cases",
          font={"size": 14,"color":"#dc9e00"},
-         xshift=-220,  # Annotation x displacement!
+         xshift=100,  # Annotation x displacement!
          showarrow=False,
     )
 
     # BAR CHART ANNOTATION
     fig_confirmed.add_annotation(
-        x=selected_df['jour'].tail(last_days).tolist()[0],
-        y=np.log10(selected_df['nb_pos'].tail(last_days).max()),
+        x=selected_df['jour'].tail(last_days_nb).tolist()[0],
+        y=np.log10(selected_df['nb_pos'].tail(last_days_nb).max()),
         text="New Confirmed Cases",
         font={"size": 14, "color":"#f9d67a"},
-        xshift=100,  # Annotation x displacement!
-        yshift=20,  # Annotation y displacement!
+        xshift=350,  # Annotation x displacement!
+        yshift=10,  # Annotation y displacement!
         showarrow=False,
         )
     fig_confirmed.update_layout(
